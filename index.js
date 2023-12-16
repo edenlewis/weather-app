487cc75e5fc963bcde55db037f6b4d75b85143ef
edenlewis <edenlewis80@gmail.com>
2023-12-15 18:18:45 -0700

    attempting to fix deployment

--- index.js
+++ index.js
@@ -1,60 +1,3 @@
-// // var repoList = document.querySelector('ul');
-// var fetchButton = document.getElementById("button-addon2");
-// // var input = document.querySelector("#search-city");
-// // var cityName = document.querySelector("#cityName");
-// // var temp = document.querySelector("#temp");
-// // var wind = document.querySelector("#wind");
-// // var humidity = document.querySelector("#humidity");
-// // var index = document.querySelector("#index");
-// // var createImg = document.createElement("img");
-// // var recentCities = JSON.parse(localStorage.getItem("saved"));
-// // var BtnPlace = document.querySelector("#BtnSpots");
-// // var searchFormEl = document.querySelector('#search-form');
-// // const myKey = ("72196e67318ffa654665dd9cccf2f44c")
-// var repoList = document.getElementById("repoList");
-
-// function start() {
-//   var searchedCity = document.getElementById("search-input").value;
-//   getData(searchedCity);
-// }
-
-// function getData(city) {
-//   forecast(city);
-//   current(city);
-// }
-
-// function forecast(city) {
-//   var requestUrl = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=72196e67318ffa654665dd9cccf2f44c`;
-//   fetch(requestUrl)
-//     .then(function (response) {
-//       return response.json();
-//     })
-//     .then(function (data) {
-//       console.log(data);
-//     });
-// }
-// function current(city) {
-//   var requestUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=72196e67318ffa654665dd9cccf2f44c`;
-//   fetch(requestUrl)
-//     .then(function (response) {
-//       return response.json();
-//     })
-//     .then(function (data) {
-//       console.log(data);
-//     });
-//     displayWeather:  
-// }
-
-// fetchButton.addEventListener("click", start);
-// // searchFormEl.addEventListener('submit', getLatLog);
-// // function getApi() {
-// //   .then(function (data){
-// //     console.log(data)
-// //   })
-// //   }
-// //   console.log("http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=72196e67318ffa654665dd9cccf2f44c")
-
-
 
 // var repoList = document.querySelector('ul');
 var fetchButton = document.getElementById("button-addon2");
@@ -63,11 +6,7 @@ var cityName = document.querySelector("#cityName");
 var temp = document.querySelector("#temp");
 var wind = document.querySelector("#wind");
 var humidity = document.querySelector("#humidity");
-// var index = document.querySelector("#index");
-// var createImg = document.createElement("img");
-// var recentCities = JSON.parse(localStorage.getItem("saved"));
-// var BtnPlace = document.querySelector("#BtnSpots");
-// var searchFormEl = document.querySelector('#search-form');
+
 // const myKey = ("72196e67318ffa654665dd9cccf2f44c")
 var repoList = document.getElementById("repoList");
 
